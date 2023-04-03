39b7e08d8267564760b4145bc4d79ad2b48d806c
so1ve <i@mk1.io>
2023-04-03 12:56:15 +0800

    refactor: extract clocpath

--- other/bump-version.mjs
+++ other/bump-version.mjs
@@ -7,6 +7,7 @@ async function main () {
     .then(res => res.json())
     .then(json => json.tag_name);
   const versionPath = path.join(process.cwd(), "other/version.txt")
+  const clocPath = path.join(process.cwd(), "lib/cloc")
   const lastCheckedVersion = await readFile(versionPath, { encoding: "utf-8" });
   if (latestVersion === lastCheckedVersion) {
     console.log("Already up to date");
@@ -16,8 +17,8 @@ async function main () {
   console.log(`Updating from ${lastCheckedVersion} to ${latestVersion}`);
   await fetch(`https://github.com/AlDanial/cloc/releases/download/${latestVersion}/cloc-${latestVersionWithoutV}.pl`)
     .then(res => res.text())
-    .then(text => writeFile("./lib/cloc", text));
-  await chmod("./lib/cloc", 0o755);
+    .then(text => writeFile(clocPath, text));
+  await chmod(clocPath, 0o755);
   await writeFile(versionPath, latestVersion);
   console.log("Done");
 }
