b87d20278ce1da4e7810b3a5962c0c038566cea9
ʀᴀʏ <i@mk1.io>
2023-04-06 00:38:49 +0800

    feat: auto bump version (#27)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ other/bump-version.mjs
@@ -0,0 +1,81 @@
+//@ts-check
+import { readFile, writeFile, chmod, access, mkdir } from "node:fs/promises";
+import path from "node:path";
+
+import { fetch } from "node-fetch-native";
+
+const cwd = process.cwd()
+const packageJsonPath = path.join(cwd, "package.json")
+const libPath = path.join(cwd, "lib");
+const clocPath = path.join(libPath, "cloc");
+const readJson = async path => JSON.parse(await readFile(path, { encoding: "utf-8" }))
+const readPackageJson = () => readJson(packageJsonPath);
+const exists = path => access(path).then(
+  () => true,
+  () => false,
+);
+/**
+ * @param {string} version
+ * Normalizes cloc version:
+ * 1.96 => 1.96.0
+ * 1.96.1 => 1.96.1
+ */
+function normalizeClocVersion (version) {
+  const parts = version.split(".");
+  if (parts.length === 2) {
+    parts.push("0");
+  }
+  return parts.join(".");
+}
+/**
+ * @param {string} normalizedVersion
+ * Converts the normalized version to original version:
+ * 1.96.0 => 1.96
+ * 1.96.1 => 1.96.1
+ */
+function normalizedClocVersionToOriginal (normalizedVersion) {
+  const parts = normalizedVersion.replace(/-cloc$/, "").split(".");
+  if (parts[2] === "0") {
+    parts.pop();
+  }
+  return parts.join(".");
+}
+
+/**
+ * @param {string} version
+ */
+async function bumpPackageJsonVersion (version) {
+  const packageJson = await readPackageJson();
+  packageJson.version = version;
+  await writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2) + "\n");
+}
+
+async function getVersionFromPackageJson () {
+  const packageJson = await readPackageJson();
+  return normalizedClocVersionToOriginal(packageJson.version);
+}
+
+async function main () {
+  const latestVersion = await fetch("https://api.github.com/repos/AlDanial/cloc/releases/latest")
+    .then(res => res.json())
+    .then(json => json.tag_name);
+  const latestVersionWithoutV = latestVersion.replace(/^v/, "");
+  const lastCheckedVersion = await getVersionFromPackageJson();
+  if (latestVersionWithoutV === lastCheckedVersion) {
+    console.log("Already up to date");
+    process.exit(1);
+  }
+  console.log(`Updating from ${"v" + lastCheckedVersion} to ${latestVersion}`);
+  if (!await exists(libPath)) {
+    await mkdir(libPath);
+  }
+  await fetch(`https://github.com/AlDanial/cloc/releases/download/${latestVersion}/cloc-${latestVersionWithoutV}.pl`)
+    .then(res => res.text())
+    .then(text => writeFile(clocPath, text));
+  await chmod(clocPath, 0o755);
+  // Cloc 
+  await bumpPackageJsonVersion(normalizeClocVersion(latestVersionWithoutV) + "-cloc");
+  console.log("Done");
+}
+
+main()
